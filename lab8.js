65feb5f5421ea1c0ba20c14e74dc8095bb29c990
Алексей Носаков <aleksnosakov@gmail.com>
2023-05-24 21:16:45 +0700

    Время

--- lab8.js
+++ lab8.js
@@ -1,11 +1,11 @@
 function showDate() {
     let out = document.getElementById('current-date');
     let today = new Date();
-    out.innerHTML +="Дата и время для локали России:" + today.toDateString('ru-RU') + "<br>";
-    out.innerHTML +="Дата и время для локали Австралии:" + today.toDateString('en-AU') + "<br>";
-    out.innerHTML +="Дата и время для локали Греции:" + today.toDateString('el-GR') + "<br>";
-    out.innerHTML +="Дата и время для локали Франции:" + today.toDateString('fr') + "<br>";
-    out.innerHTML +="Дата и время для локали Италии:" + today.toDateString('it') + "<br>";
+    out.innerHTML +="Дата и время для локали России:" + today.toLocaleDateString('ru-RU') + "," + today.toLocaleTimeString('ru-Ru') + "<br>";
+    out.innerHTML +="Дата и время для локали Австралии:" + today.toLocaleDateString('en-AU') + "," + today.toLocaleTimeString('en-AU') + "<br>";
+    out.innerHTML +="Дата и время для локали Греции:" + today.toLocaleDateString('el-GR') +  "," + today.toLocaleTimeString('el-GR') + "<br>";
+    out.innerHTML +="Дата и время для локали Франции:" + today.toLocaleDateString('fr') + "," + today.toLocaleTimeString('fr') +  "<br>";
+    out.innerHTML +="Дата и время для локали Италии:" + today.toLocaleDateString('it') + "," + today.toLocaleTimeString('it') +  "<br>";
 }
 
 function showDaysCount() {
@@ -15,5 +15,12 @@ function showDaysCount() {
     let daysCount = (today - birtday)/1000/60/60/24;
     let div = document.getElementById('div1')
     daysCount = Math.floor(daysCount);
-    div.innerHTML = "Количество дней сдаты рождения:" + daysCount
+    div.innerHTML = "Количество дней с даты рождения:" + daysCount
 }
+
+function showTime() {
+    let today = new Date();
+    let currentTime = today.toLocaleTimeString('ru-RU');
+    document.getElementById('time').innerHTML = currentTime;
+}
+setInterval (showTime, 1000);
